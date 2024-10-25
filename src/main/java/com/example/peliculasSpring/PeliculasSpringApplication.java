655f9d42f5357f19e95372cca51adb86eaa281cb
Joel <evilian2000@gmail.com>
2024-10-25 17:03:52 +0200

    Initial commit: Back basico

---
+++ src/main/java/com/example/peliculasSpring/PeliculasSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.peliculasSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeliculasSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeliculasSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/peliculasSpring/PeliculasSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.peliculasSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeliculasSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
